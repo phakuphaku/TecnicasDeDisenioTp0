e3d8c425bc645718bf1892fd0b8921e5d93d1cb0
phakuphaku <phakuphaku@hotmail.com>
2015-03-15 19:01:04 -0300

    subo la ultima clase, ya que corri los tests exitosamente

--- src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
+++ src/main/java/ar/fiuba/tecnicas/rockpaperscissors/Scissors.java
@@ -1,5 +1,7 @@
 package main.java.ar.fiuba.tecnicas.rockpaperscissors;
 
 public class Scissors {
-
+	public Scissors vs(Paper paper) {return this;}
+	public Rock vs(Rock rock) {return rock;}
+	public Scissors vs(Scissors scissors) {return scissors;}
 }
